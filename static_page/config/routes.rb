fc8c741f20aa2a1a9ead3c13f3904e6f1bcd8f21
HienVu2401 <hienvu2401@gmail.com>
2016-10-10 20:50:24 +0700

    basic login

--- static_page/config/routes.rb
+++ static_page/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'users/new'
 
   get 'users/show'
@@ -14,6 +15,10 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
 
   post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   root 'static_pages#home'
 
